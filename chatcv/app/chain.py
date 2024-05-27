c1af13a3ad28960b3d67f3e364f81b6792d669d2
lorenzbaraldi <lorenz.baraldi@gmail.com>
2024-05-27 15:27:27 +0200

    Added first demo

---
+++ chatcv/app/chain.py
@@ -0,0 +1,102 @@
+import os
+import getpass
+import bs4
+from langchain import hub
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+import dotenv
+from llama_index.core import SimpleDirectoryReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+
+
+from operator import itemgetter
+from typing import List, Tuple
+
+from fastapi import FastAPI
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, format_document
+from langchain_core.runnables import RunnableMap, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langserve import add_routes
+from langserve.pydantic_v1 import BaseModel, Field
+
+from prompts import DEFAULT_DOCUMENT_PROMPT, CONDENSE_QUESTION_PROMPT, ANSWER_PROMPT
+# Load the OpenAI API and LangChain key from the environment
+dotenv.load_dotenv()
+# User input
+class ChatHistory(BaseModel):
+    """Chat history with the bot."""
+
+    chat_history: List[Tuple[str, str]] = Field(
+        ...,
+        extra={"widget": {"type": "chat", "input": "question"}},
+    )
+    question: str
+
+def _combine_documents(docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"):
+    """Combine documents into a single string."""
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+def _format_chat_history(chat_history: List[Tuple]) -> str:
+    """Format chat history into a string."""
+    buffer = ""
+    for dialogue_turn in chat_history:
+        human = "Human: " + dialogue_turn[0]
+        ai = "Assistant: " + dialogue_turn[1]
+        buffer += "\n" + "\n".join([human, ai])
+    return buffer
+
+def calculate_embeddings_rag():
+    '''
+        Calculate embeddings for rag, open folder media and recalculate all embeddings
+    '''
+    reader = SimpleDirectoryReader(
+        input_dir="media/"
+    )
+    docs = reader.load_data()
+    print(f"Loaded {len(docs)} docs")
+    documents = [doc.to_langchain_format() for doc in docs]
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(documents)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory="./chroma_db")
+    
+
+def get_chain():
+     
+    vectorstore=Chroma(persist_directory="./chroma_db",)
+    retriever = vectorstore.as_retriever()
+
+    _inputs = RunnableMap(
+        standalone_question=RunnablePassthrough.assign(
+            chat_history=lambda x: _format_chat_history(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | ChatOpenAI(model="gpt-3.5-turbo-0125")
+        | StrOutputParser(),
+    )
+
+    _context = {
+        "context": itemgetter("standalone_question") | retriever | _combine_documents,
+        "question": lambda x: x["standalone_question"],
+    }
+    conversational_qa_chain = (
+        _inputs | _context | ANSWER_PROMPT | ChatOpenAI() | StrOutputParser()
+    )
+    chain = conversational_qa_chain.with_types(input_type=ChatHistory)
+    return chain
+
+    # response = rag_chain.invoke({"input": "Who is Lorenzo Baraldi?"})
+    # response["answer"]
---
+++ chatcv/app/prompts.py
@@ -0,0 +1,19 @@
+
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, format_document
+_TEMPLATE = """Given the following conversation and a follow up question, rephrase the 
+follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_TEMPLATE)
+
+ANSWER_TEMPLATE = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+ANSWER_PROMPT = ChatPromptTemplate.from_template(ANSWER_TEMPLATE)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
